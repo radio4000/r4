80f642296cd506afbe16c79ab6b880a48f8a59ad
Oskar Rough <oskarrough@gmail.com>
2016-03-12 17:18:25 +0100

    [refactor] avoid filtering ids by prepending the domain

--- src/find-tracks.js
+++ src/find-tracks.js
@@ -1,16 +1,19 @@
 /* global document */
-
 const Nightmare = require('nightmare');
 
-module.exports = url => {
-	const n = new Nightmare({show: false})
+const findTracks = (url) => {
+	return new Nightmare({show: false})
 		.goto(url)
 		.wait('.Channel-outlet')
 		.wait(6000) // Waiting for .Track is not reliable. Neither is this. But yea.
-		// .wait('.Track')
 		.evaluate(() => {
-			return Array.from(document.querySelectorAll('.Track')).map(t => t.getAttribute('data-pid'));
+			const elements = document.querySelectorAll('.Track');
+			// YouTube IDs are stored as `data-pid` attributes.
+			// A full URL is needed (https://github.com/rg3/youtube-dl/#how-do-i-download-a-video-starting-with-a--)
+			const ids = Array.from(elements).map(e => `http://www.youtube.com/watch?v=${e.getAttribute('data-pid')}`);
+			return ids;
 		})
 		.end();
-	return n;
 };
+
+module.exports = findTracks;
