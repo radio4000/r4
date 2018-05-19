0f26e9dce75cae4a644fd189a45e3713376a3654
Oskar Rough <oskar@rough.dk>
2018-05-19 21:18:36 +0200

    Make it monochrome

--- r4.js
+++ r4.js
@@ -9,7 +9,9 @@ args
 		description: `To download the channel at https://radio4000.com/200ok`
 	}])
 
-const flags = args.parse(process.argv)
+const flags = args.parse(process.argv, {
+	mainColor: ['black']
+})
 const command = args.sub[0]
 
 if (!command) {
