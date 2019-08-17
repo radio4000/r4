a82a62395e8e4b4a5ce60f7a4982a60f99ca68ef
hugurp <hugurp@protonmail.com>
2019-08-17 18:11:54 +0200

    save a json of the channel.backup in the channel folder

--- r4-download.js
+++ r4-download.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const args = require('args')
+var fs = require('fs-extra')
 const {createBackup} = require('radio4000-sdk')
 const commandExists = require('command-exists')
 const downloadTracks = require('./lib/download-tracks')
@@ -22,6 +23,10 @@ let slug = args.sub[0]
 // If the "dl" alias is used, slug is second argument.
 if (slug === 'dl') slug = args.sub[1]
 
+const saveChannelData = async (channelData, backupPath) => {
+	return fs.outputFile(backupPath, JSON.stringify(channelData))
+}
+
 const main = async function() {
 	try {
 		await commandExists('youtube-dl')
@@ -41,21 +46,52 @@ const main = async function() {
 		return
 	}
 
+	try {
+		await require('dns').lookup('wikipedia.org', error => {
+			if (error && error.code == "ENOTFOUND") {
+        return error
+      }
+			return true
+		})
+	} catch (error) {
+		console.warn('Error: there is no internet connection. It is required for this command')
+		return
+	}
+
 	if (flags.search) {
 		slug = await autocompleteChannels()
 	}
 
 	if (!slug) args.showHelp()
 
-	console.log(`Starting downloading channel: ${slug}`)
+	console.log(`Starting download for channel: ${slug}`)
 
+	// first get the backup
+	let backup
 	try {
-		const backup = await createBackup(slug)
-		await downloadTracks(backup.tracks, slug)
-		console.log(`Download complete for channel: ${slug}`)
+		backup = await createBackup(slug)
+	} catch (error) {
+		console.warn(error)
+		return
+	}
+
+	// Save the channel.backup json information to a file
+	try {
+		const backupPath = `./${slug}/${slug}.json`
+		await saveChannelData(backup, backupPath)
+		console.log(`Saved channel information as text: ${backupPath}`)
 	} catch (error) {
 		console.warn(error)
 	}
+
+	// Save the channels media to files
+	try {
+		await downloadTracks(backup.tracks, slug)
+		console.log(`Finished download for channel: ${slug}`)
+	} catch (error) {
+		console.warn(error)
+	}
+
 }
 
 main()
