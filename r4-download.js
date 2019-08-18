5fd5ab382ca5d19da2fe8d7322a91b8a14e3f458
hugurp <hugurp@protonmail.com>
2019-08-18 14:03:57 +0200

    fix metadata

--- r4-download.js
+++ r4-download.js
@@ -80,7 +80,7 @@ const main = async function() {
 	// first get the backup
 	let backup
 	try {
-		debugOutput && console.log('Fetching data @ api.radio4000.com for')
+		debugOutput && console.log('Fetching data @ api.radio4000.com')
 		backup = await createBackup(slug)
 	} catch (error) {
 		console.warn(error)
